399cb323575119b44a682e959bce270127f9fc57
Thiago Santos <tdos233@gmail.com>
2021-05-28 12:26:00 -0300

    Faltando ultimo exemplo e refinamento

--- interface.py
+++ interface.py
@@ -1,12 +1,24 @@
 from tkinter import *
 from tkinter import scrolledtext
 from BidirecionalSearch import BidirectionalSearch
+import networkx as nx
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 class Application:
     def __init__(self, master=None):
         self.fontePadrao = ("Arial", "10")
         self.primeiroContainer = Frame(master)
         self.primeiroContainer["pady"] = 10
         self.primeiroContainer.pack()
+
+        self.InstrucoesContainer = Frame(master)
+        self.InstrucoesContainer["pady"] = 10
+        self.InstrucoesContainer.pack()
 
         self.segundoContainer = Frame(master)
         self.segundoContainer["padx"] = 20
@@ -15,14 +27,36 @@ class Application:
         self.terceiroContainer = Frame(master)
         self.terceiroContainer["padx"] = 20
         self.terceiroContainer.pack()
+        
+        self.TextoContainer = Frame(master)
+        self.TextoContainer["padx"] = 20
+        self.TextoContainer.pack()
+        
+        self.exemplosContainer = Frame(master)
+        self.exemplosContainer["padx"] = 20
+        self.exemplosContainer.pack()
+
 
         self.quartoContainer = Frame(master)
         self.quartoContainer["pady"] = 20
         self.quartoContainer.pack()
 
-        self.titulo = Label(self.primeiroContainer, text="Dados do Grafo")
+        self.titulo = Label(self.primeiroContainer, text="Busca Bidirecional")
         self.titulo["font"] = ("Arial", "10", "bold")
         self.titulo.pack()
+
+        self.instrucoes = Label(self.InstrucoesContainer, text="""1º-preencha o numero de vertices desejado. Os vertices são representado pelos numeros do intervalo 0 a o numero de vertices -1.
+        2º-No campo arestas preencha com os vertices de cada aresta da seguinte forma numero do vertice,numero do vertice.Cada linha representa uma aresta, entao ao terminar uma aresta precione enter.
+        3º-Preencha o vertice de inicio e destino, lembrando que os vertices tem que estar detro do intervalo de 0 a numero de vertice -1
+        4º-Para inserir as entradas dos exemplo é só clicar no botão referente ao exemplo.
+        5º- para fazer a busca click no botão Buscar e uma imagem do grafo será exibida abaixo do botão.
+        
+        Os vertices em vermelho representam o passeio/percuso pecorrido pela busca.
+        Caso não apareça em vermelho é por que o caminho é o proprio grafo.
+        Caso a imagem dé a impressão de estar errada ou não esteja dando para ver direito click em Buscar novamente.""")
+        self.instrucoes["font"] = ("Arial", "10", "bold")
+        self.instrucoes.pack()
+
 
         self.numeroLabel = Label(self.segundoContainer,text="numero de vertices", font=self.fontePadrao)
         self.numeroLabel.pack(side=LEFT)
@@ -49,46 +83,115 @@ class Application:
         self.dest=Entry(self.terceiroContainer,width=5,font=self.fontePadrao)
         self.dest.pack(side=LEFT)
         self.dest.insert(0,14)
-        # self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
-        # self.senhaLabel.pack(side=LEFT)
+        
 
-        # self.senha = Entry(self.terceiroContainer)
-        # self.senha["width"] = 30
-        # self.senha["font"] = self.fontePadrao
-        # self.senha["show"] = "*"
-        # self.senha.pack(side=LEFT)
+        self.buscar = Button(self.quartoContainer)
+        self.buscar["text"] = "Buscar"
+        self.buscar["font"] = ("Calibri", "8")
+        self.buscar["width"] = 12
+        self.buscar["command"] = self.Buscar
+        self.buscar.pack()
 
-        self.autenticar = Button(self.quartoContainer)
-        self.autenticar["text"] = "Buscar"
-        self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
-        self.autenticar["command"] = self.Buscar
-        self.autenticar.pack()
+        self.exemplo1 = Button(self.exemplosContainer)
+        self.exemplo1["text"] = "Exemplo 1"
+        self.exemplo1["font"] = ("Calibri", "8")
+        self.exemplo1["width"] = 12
+        self.exemplo1["command"] = self.exemplo_1
+        self.exemplo1.pack(side=LEFT)
+
+        self.exemplo2 = Button(self.exemplosContainer)
+        self.exemplo2["text"] = "Exemplo 2"
+        self.exemplo2["font"] = ("Calibri", "8")
+        self.exemplo2["width"] = 12
+        self.exemplo2["command"] = self.exemplo_2
+        self.exemplo2.pack(side=LEFT)
+
+        self.exemplo3 = Button(self.exemplosContainer)
+        self.exemplo3["text"] = "Exemplo 3"
+        self.exemplo3["font"] = ("Calibri", "8")
+        self.exemplo3["width"] = 12
+        self.exemplo3["command"] = self.exemplo_3
+        self.exemplo3.pack(side=LEFT)
 
         self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
         self.mensagem.pack()
+    def exemplo_1(self):
+        self.nvertice.delete(0,END)
+        self.nvertice.insert(0,15)
+        self.aresta.delete('1.0',END)
+        self.aresta.insert('1.0',"0,4\n1,4\n2,5\n3,5\n4,6\n5,6\n6,7\n7,8\n8,9\n8,10\n9,11\n9,12\n10,13\n10,14")
+        self.scr.delete(0,END)
+        self.scr.insert(0,0)
+        self.dest.delete(0,END)
+        self.dest.insert(0,14)
+        print('ex1')
+    def exemplo_2(self):
+        self.nvertice.delete(0,END)
+        self.nvertice.insert(0,2)
+        self.aresta.delete('1.0',END)
+        self.aresta.insert('1.0',"0,1")
+        self.scr.delete(0,END)
+        self.scr.insert(0,0)
+        self.dest.delete(0,END)
+        self.dest.insert(0,1)
+        print('ex2')
+    def exemplo_3(self):
+        self.nvertice.delete(0,END)
+        self.nvertice.insert(0,15)
+        self.aresta.delete('1.0',END)
+        self.aresta.insert('1.0',"0,4\n1,4\n2,5\n3,5\n4,6\n5,6\n6,7\n7,8\n8,9\n8,10\n9,11\n9,12\n10,13\n10,14")
+        self.scr.delete(0,END)
+        self.scr.insert(0,0)
+        self.dest.delete(0,END)
+        self.dest.insert(0,14)
+        print('ex3')
+    
 
-    #Método verificar senha
-    def verificaSenha(self):
-        usuario = self.nome.get()
-        senha = self.senha.get()
-        if usuario == "usuariodevmedia" and senha == "dev":
-            self.mensagem["text"] = "Autenticado"
-        else:
-            self.mensagem["text"] = "Erro na autenticação"
     def Buscar(self):
+        print("busca")
         n=int(self.nvertice.get())
-        self.grafo_busca=BidirectionalSearch(n)
+        grafo_busca=BidirectionalSearch(n)
         arestas=self.aresta.get("1.0",END)[:-1]
         scr=int(self.scr.get())
         dest=int(self.dest.get())
+        G = nx.Graph()
+        for i in range(n):
+            G.add_node(i)
         for arest in arestas.split('\n'):
-            arest.split(',')
-            self.grafo_busca.add_edge(int(arest.split(',')[0]), int(arest.split(',')[1]))
-        path,intersecting_node =self.grafo_busca.bidirectional_search(scr, dest)
+            a=int(arest.split(',')[0])
+            b=int(arest.split(',')[1])
+            G.add_edge(a, b)
+            grafo_busca.add_edge(a, b)
+        path,intersecting_node =grafo_busca.bidirectional_search(scr, dest)
         if(path==-1):
             path="não existe caminho entre os vertices {} e {}".format(str(scr),str(dest))
-        self.mensagem["text"]=str(path)
+            self.mensagem.configure(image='')
+            self.mensagem["text"]=str(path)
+        else:
+            val_map ={}
+            values = []
+
+            colorirvermelho=path[:path.index(str(intersecting_node))].split(' ')
+            colorirvermelho.pop(colorirvermelho.index(''))
+            colorirVerde=path[path.index(str(intersecting_node))+1:].split(' ')
+            colorirVerde.pop(colorirVerde.index(''))
+            for i in range(len(colorirvermelho)):
+                val_map[int(colorirvermelho[i])]=0.5714285714285714
+            for i in range(len(colorirVerde)):
+                val_map[int(colorirVerde[i])]=0.5714285714285714
+            val_map[int(intersecting_node)]=0.5714285714285714
+            values = [val_map.get(node,0.5) for node in G.nodes()]
+            nx.draw(G, with_labels=True, cmap=plt.get_cmap('jet'), node_color=values,edge_color="skyblue", style="solid")
+            plt.savefig('imd.png')
+            plt.close()
+            ima=PhotoImage(file='imd.png')
+            self.mensagem.configure(image=ima)
+            self.mensagem.imagem=ima
+            
+            
+            
+
+    
         
         
 
